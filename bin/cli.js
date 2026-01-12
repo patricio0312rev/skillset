e33b52edf59b08e9ddb063635711620d3be03a9a
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-01-11 19:55:15 -0500

    refactor: implement new commands on cli

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ const chalk = require("chalk");
 const packageJson = require("../package.json");
 const { displayBanner } = require("../src/utils/display");
 const initCommand = require("../src/commands/init");
+const searchCommand = require("../src/commands/search");
+const listCommand = require("../src/commands/list");
 
 // Display banner
 displayBanner();
@@ -28,6 +30,19 @@ program
   .option("--skills <list>", "Comma-separated list of specific skills")
   .option("--skip-prompts", "Skip all interactive prompts (requires all flags)")
   .action(initCommand);
+
+program
+  .command("search <query>")
+  .description("Search for skills by name")
+  .option("-g, --grouped", "Group results by domain")
+  .action(searchCommand);
+
+program
+  .command("list")
+  .description("List all available skills")
+  .option("-d, --domain <name>", "Filter by domain")
+  .option("-c, --compact", "Show only domain names, not individual skills")
+  .action(listCommand);
 
 program.parse(process.argv);
 
